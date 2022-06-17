86bbb679aa6fdc6666b752228ef3f8ca43d932bf
Suyeong Rhie <10545351+noxowl@users.noreply.github.com>
2022-06-18 02:45:54 +0900

    fix indent

--- l1.py
+++ l1.py
@@ -6,18 +6,18 @@ factorial = lambda x: x * factorial(x - 1) if (x > 0) else 1
 
 
 def factorial_test() -> None:
-	print('Test factorial for 1')
-	assert factorial(1) == 1
-	print('OK')
-	print('Test factorial for 2')
-	assert factorial(2) == 2
-	print('OK')
-	print('Test factorial for 4')
-	assert factorial(4) == 24
-	print('OK')
-	print('Test factorial for 5')
-	assert factorial(5) == 120
-	print('OK')
+    print('Test factorial for 1')
+    assert factorial(1) == 1
+    print('OK')
+    print('Test factorial for 2')
+    assert factorial(2) == 2
+    print('OK')
+    print('Test factorial for 4')
+    assert factorial(4) == 24
+    print('OK')
+    print('Test factorial for 5')
+    assert factorial(5) == 120
+    print('OK')
 
 
 """
@@ -27,18 +27,18 @@ fib = lambda n: n if n < 2 else fib(n - 1) + fib(n - 2)
 
 
 def fib_test() -> None:
-	print('Test fib for 0')
-	assert fib(0) == 0
-	print('OK')
-	print('Test fib for 2')
-	assert fib(2) == 1
-	print('OK')
-	print('Test fib for 3')
-	assert fib(3) == 2
-	print('OK')
-	print('Test fib for 7')
-	assert fib(7) == 13
-	print('OK')
+    print('Test fib for 0')
+    assert fib(0) == 0
+    print('OK')
+    print('Test fib for 2')
+    assert fib(2) == 1
+    print('OK')
+    print('Test fib for 3')
+    assert fib(3) == 2
+    print('OK')
+    print('Test fib for 7')
+    assert fib(7) == 13
+    print('OK')
 
 
 factorial_test()
--- l2.py
+++ l2.py
@@ -8,12 +8,12 @@ average = lambda l: reduce(lambda x, y: x + y, l) / len(l)
 
 
 def average_test() -> None:
-	print('Test average for [1, 2, 3, 4]')
-	assert average([1, 2, 3, 4]) == 2.5
-	print('OK')
-	print('Test average for [0, 2, 3, 4]')
-	assert average([0, 2, 3, 4]) == 2.25
-	print('OK')
+    print('Test average for [1, 2, 3, 4]')
+    assert average([1, 2, 3, 4]) == 2.5
+    print('OK')
+    print('Test average for [0, 2, 3, 4]')
+    assert average([0, 2, 3, 4]) == 2.25
+    print('OK')
 
 
 """
@@ -23,9 +23,9 @@ double([x1,x2,...,xn]) = [2x1,2x2,...,2xn]
 cal_double = lambda l: [x * 2 for x in l]
 
 def double_test() -> None:
-	print('Test cal_double for [1, 2, 3, 4]')
-	assert cal_double([1, 2, 3, 4]) == [2, 4, 6, 8]
-	print('OK')
+    print('Test cal_double for [1, 2, 3, 4]')
+    assert cal_double([1, 2, 3, 4]) == [2, 4, 6, 8]
+    print('OK')
 
 
 """
@@ -35,8 +35,8 @@ Implement matrix addition.
 
 
 def matrix_addition_test() -> None:
-	pass
-	# assert matrix_addition([[1, 2], [3, 4]], [[1, 2], [3, 4]])
+    pass
+    # assert matrix_addition([[1, 2], [3, 4]], [[1, 2], [3, 4]])
 
 
 """
@@ -47,9 +47,9 @@ reverse = lambda l: [l.pop() for i in range(len(l))]
 
 
 def reverse_test() -> None:
-	print('Test reverse for [1, 2, 3]')
-	assert reverse([1, 2, 3]) == [3, 2, 1]
-	print('OK')
+    print('Test reverse for [1, 2, 3]')
+    assert reverse([1, 2, 3]) == [3, 2, 1]
+    print('OK')
 
 
 """
@@ -61,13 +61,13 @@ palindrome = lambda l: ''.join(l) == ''.join(l[::-1])
 
 
 def palindrome_test() -> None:
-	print('Test palindrome for ["a", "n", "n", "a"]')
-	assert palindrome(["a", "n", "n", "a"]) == True
-	print('OK')
+    print('Test palindrome for ["a", "n", "n", "a"]')
+    assert palindrome(["a", "n", "n", "a"]) == True
+    print('OK')
 
-	print('Test palindrome for ["a", "p", "p", "l", "e"]')
-	assert palindrome(["a", "p", "p", "l", "e"]) == False
-	print('OK')
+    print('Test palindrome for ["a", "p", "p", "l", "e"]')
+    assert palindrome(["a", "p", "p", "l", "e"]) == False
+    print('OK')
 
 
 """
@@ -81,17 +81,17 @@ ascending = lambda l: next(filter(lambda x: x == False, [l[i] <= l[i + 1 if i < 
 
 
 def ascending_test() -> None:
-	print('Test ascending for [2, 4, 7]')
-	assert ascending([2, 4, 7]) == True
-	print('OK')
+    print('Test ascending for [2, 4, 7]')
+    assert ascending([2, 4, 7]) == True
+    print('OK')
 
-	print('Test ascending for [9, 3, 7, 7, 9]')
-	assert ascending([9, 3, 7, 7, 9]) == False
-	print('OK')
-	
-	print('Test ascending for [2, 4, 7, 3]')
-	assert ascending([2, 4, 7, 3]) == False
-	print('OK')
+    print('Test ascending for [9, 3, 7, 7, 9]')
+    assert ascending([9, 3, 7, 7, 9]) == False
+    print('OK')
+    
+    print('Test ascending for [2, 4, 7, 3]')
+    assert ascending([2, 4, 7, 3]) == False
+    print('OK')
 
 
 """
@@ -102,9 +102,9 @@ maximum = lambda l: reduce(lambda x, y: x if (x > y) else y, l)
 
 
 def maximum_test() -> None:
-	print('Test maximum for [1, 2, 3]')
-	assert maximum([1, 2, 3]) == 3
-	print('OK')
+    print('Test maximum for [1, 2, 3]')
+    assert maximum([1, 2, 3]) == 3
+    print('OK')
 
 
 """
@@ -119,25 +119,25 @@ join([”a”, ”b”, ”c”], ” + ”) = ”a + b + c”
 join = lambda x, y: f'{y}'.join(x)
 
 def join_test() -> None:
-	print('Test join for ["123", "45", "67"] and "ab"')
-	assert join(["123", "45", "67"], "ab") == "123ab45ab67"
-	print('OK')
-	
-	print('Test join for [] and ","')
-	assert join([], ",") == ""
-	print('OK')
-	
-	print('Test join for ["a"] and "+"')
-	assert join(["a"], "+") == "a"
-	print('OK')
-	
-	print('Test join for ["a", "b"] and "+"')
-	assert join(["a", "b"], "+") == "a+b"
-	print('OK')
-	
-	print('Test join for ["a", "b", "c"], "+"')
-	assert join(["a", "b", "c"], "+") == "a+b+c"
-	print('OK')
+    print('Test join for ["123", "45", "67"] and "ab"')
+    assert join(["123", "45", "67"], "ab") == "123ab45ab67"
+    print('OK')
+    
+    print('Test join for [] and ","')
+    assert join([], ",") == ""
+    print('OK')
+    
+    print('Test join for ["a"] and "+"')
+    assert join(["a"], "+") == "a"
+    print('OK')
+    
+    print('Test join for ["a", "b"] and "+"')
+    assert join(["a", "b"], "+") == "a+b"
+    print('OK')
+    
+    print('Test join for ["a", "b", "c"], "+"')
+    assert join(["a", "b", "c"], "+") == "a+b+c"
+    print('OK')
 
 
 average_test()
