ac179dd3e52732b5727c8171b204e2837970da7a
Suyeong Rhie <10545351+noxowl@users.noreply.github.com>
2022-06-18 02:11:45 +0900

    add l3 scaffold

---
+++ l3.py
@@ -0,0 +1,58 @@
+"""
+Implement the membership function for lists: 
+							   
+member(x, [y1, y2, ..., yn]) = True if x = yi for some i
+							   False otherwise
+"""
+
+
+"""
+gcd(x, 0) = x
+gcd(x, y) = gcd(y, x % y) if y > 0
+
+Example
+gcd(24, 32) = gcd(32, 24) = gcd(24, 8) = gcd(8, 0) = 8
+
+in light of above observation, implement gcd
+1 by using recursion
+2 by using while-loop
+"""
+
+
+"""
+Use recursion to reimplement the bisection algorithm.
+if f is continuous, a < b, and f(a)f(b) < 0 then
+f(x) = 0 / for some a < x < b
+
+given function f and a0, b0 ∈ R with a0 < b0, we define
+			   
+(an+1, bn+1) = (an, cn) if f(an)f(cn) < 0
+			   (cn, bn) otherwise
+where cn = (an + bn) / 2
+
+
+def f(x):
+	return x * x - 2
+
+
+def bisection(a, b):
+	e = 0.00000001
+	while abs(a - b) > e:
+		c = (a + b) / 2
+		if f(a) * f(c) <= 0:
+			b = c
+		else:
+			a = c
+	return (a + b) / 2
+
+
+print(bisection(0,3)) # 1.41421355959028
+"""
+
+
+"""
+Use the bisection method to implement root(z) that approximates √z, where z 0.
+Hint: Given a non-negative real number z, you have to find numbers a and b such that a <= √z <= b holds. 
+You may take a = 0. What about b? Use z to construct an upper-bound b of √z.
+"""
+
