29cfb2c085a42b091c40bbc547f4fab8cecd59fb
Suyeong RHIE <10545351+noxowl@users.noreply.github.com>
2022-06-29 19:30:03 +0900

    add more examples

--- l1.py
+++ l1.py
@@ -8,6 +8,15 @@ Lecture 1
 """
 
 
+def python_test():
+    """
+    Set up Python and editor. Important!
+    """
+    print('Can you execute this code?')
+    print('Hello world!')
+    print('OK')
+
+
 factorial = lambda x: x * factorial(x - 1) if (x > 0) else 1
 
 
@@ -51,5 +60,131 @@ def fib_test() -> None:
     print('OK')
 
 
+def for_loop():
+    x = 1
+    s = 1
+    for i in range(0, 4):
+        x = x + x * s
+        s = - s
+    return x, s
+
+
+def unfold_for_loop():
+    x = 1
+    # x = 1
+    s = 1
+    # x = 1, s = 1
+    # loop 0
+    x = 1 + 1 * 1
+    # x = 2, s = 1
+    s = - 1
+    # x = 2, s = -1
+    # loop 1
+    x = 2 + 2 * -1
+    # x = 0, s = -1
+    s = - -1
+    # x = 0, s = 1
+    # loop 2
+    x = 0 + 0 * 1
+    # x = 0, s = 1
+    s = - 1
+    # x = 0, s = -1
+    # loop 3
+    x = 0 + 0 * 1
+    # x = 0, s = -1
+    s = - -1
+    # x = 0, s = 1
+    return x, s
+
+
+def for_loop_with_analyzing():
+    x = 1
+    # x = 1
+    s = 1
+    # x = 1, s = 1
+    # loop 0
+    x = x + x * s
+    # x = 2, s = 1
+    s = - s
+    # x = 2, s = -1
+    # loop 1
+    x = x + x * s
+    # x = 0, s = -1
+    s = - s
+    # x = 0, s = 1
+    # loop 2
+    x = x + x * s
+    # x = 0, s = 1
+    s = - s
+    # x = 0, s = -1
+    # loop 3
+    x = x + x * s
+    # x = 0, s = -1
+    s = - s
+    # x = 0, s = 1
+    return x, s
+
+
+def unfold_for_loop_test():
+    """
+    Consider the following program:
+        x=1
+        s=1
+        for i in range(0, 4):
+            x = x + x * s
+            s = - s
+    1. Unfold for-loop, as we did on slide 20.
+    slide 20:
+        for i in range(0, 4):
+            x = i * 10
+            print(i, x)
+        is unfolded to equivalent code:
+            x = 0 * 10
+            print(0, x)
+            x = 1 * 10
+            print(1, x)
+            x = 2 * 10
+            print(2, x)
+            x = 3 * 10
+            print(3, x)
+    2. Analyze variable assignments for the unfolded code, as we did on slide 16.
+    slide 16:
+        x=1
+        #x=1
+        y=2
+        # x = 1, y = 2
+        z=x
+        # x = 1, y = 2, z = 1
+        x=y
+        # x = 2, y = 2, z = 1
+        y=z
+        # x = 2, y = 1, z = 1
+        print(x, y)
+    :return: None
+    """
+    print('Test unfold_for_loop')
+    assert unfold_for_loop() == for_loop()
+    print('OK')
+
+
+leibniz = lambda x: x # later
+
+
+def leibniz_test():
+    """
+    Recall the Leibniz formula:
+        π = 4−4/3+4/5−4/7+4/9−4/11 + ···
+    Implement leibniz(n) that sums up the first n fractional numbers in the formula. For instance,
+        leibniz(4) = 4-4/3+4/5-4/7
+    :return: None
+    """
+    print('Test leibniz for 4')
+    leibniz(4)
+    print('OK')
+
+
+python_test()
 factorial_test()
 fib_test()
+unfold_for_loop_test()
+leibniz_test()
